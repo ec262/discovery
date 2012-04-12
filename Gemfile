8141800649ee5e83f351e277593d2b23dacfc396
Jonathan Levine <tlozoot@gmail.com>
2012-04-12 01:54:19 -0400

    minimal minimalist

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source :rubygems
 gem 'sinatra', '1.3.2'
-gem 'thin'
-gem 'redis'
-gem 'json'
+# gem 'thin'
+# gem 'redis'
+# gem 'json'
---
+++ discovery.rb
@@ -0,0 +1,25 @@
+require 'sinatra'
+
+# configure do
+#   require 'redis'
+#   uri = URI.parse(ENV["REDISTOGO_URL"])
+#   REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+#   puts "Setup Redis with host #{ uri.to_s }"
+# end
+
+get '/' do
+  "Hello world"
+end
+
+# post '/register' do
+#   REDIS.sadd("workers", request.ip)
+# end
+# 
+# get '/workers' do
+#   all_workers = REDIS.smembers("workers")
+#   live_workers = []
+#   all_workers.each do |w|
+#     # TODO: 
+#   end
+#   live_workers.to_json
+# end
