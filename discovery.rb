fa428351f43bf7d7521a74fd03109825337c0a9f
Jonathan Levine <tlozoot@gmail.com>
2012-04-12 00:53:03 -0400

    version 0

--- discovery.rb
+++ discovery.rb
@@ -1,5 +1,25 @@
 require 'sinatra'
+require 'json'
 
-get '/hi' do
-  "Hello World!"
+configure do
+  require 'redis'
+  uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://127.0.0.1:6379")
+  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+end
+
+get '/' do
+  "Hello world"
+end
+
+post '/register' do
+  REDIS.sadd("workers", request.ip)
+end
+
+get '/workers' do
+  all_workers = REDIS.smembers("workers")
+  live_workers = []
+  all_workers.each do |w|
+    # TODO: 
+  end
+  live_workers.to_json
 end
