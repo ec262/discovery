2629f2c8395025ef94165e48e686ff16c4c9bfd1
Jonathan Levine <tlozoot@gmail.com>
2012-04-12 01:34:02 -0400

    cleaner

--- discovery.rb
+++ discovery.rb
@@ -1,10 +1,10 @@
 require 'sinatra'
-require 'json'
 
 configure do
   require 'redis'
   uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://127.0.0.1:6379")
   REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+  puts "Setup Redis with host #{ uri.to_s }"
 end
 
 get '/' do
